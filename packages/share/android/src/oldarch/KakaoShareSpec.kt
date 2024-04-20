e18d9a15fd7c59f1265b443955a3f2ecb55aa402
mym0404 <mym0404@gmail.com>
2024-04-20 20:23:52 +0900

    fix(android): fix spec old arch type

--- packages/share/android/src/oldarch/KakaoShareSpec.kt
+++ packages/share/android/src/oldarch/KakaoShareSpec.kt
@@ -2,10 +2,6 @@ package net.mjstudio.rnkakao.share
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Promise
 
 abstract class KakaoShareSpec internal constructor(context: ReactApplicationContext) :
-  ReactContextBaseJavaModule(context) {
-
-  abstract fun multiply(a: Double, b: Double, promise: Promise)
-}
+    ReactContextBaseJavaModule(context)
