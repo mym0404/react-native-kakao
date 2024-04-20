990769b5f528355bf3b71b84bf6ab92d0e8b4d59
mym0404 <mym0404@gmail.com>
2024-04-20 12:29:10 +0900

    chore: configure project

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,110 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const title = 'React Native Kakao';
+const description = 'Native Kakao SDK All In One Solution';
+const websiteUrl = 'https://mj-studio-library.github.io/react-native-kakao/';
+const repoOrg = 'mj-studio-library';
+const repoName = 'react-native-kakao';
+const repoUrl = 'https://github.com/mj-studio-library/react-native-kakao';
+const coverImage = 'img/social-card.png';
+
+const config: Config = {
+  title: title,
+  tagline: description,
+  favicon: 'img/favicon.png',
+
+  url: 'https://mj-studio-library.github.io',
+  baseUrl: '/react-native-kakao/',
+
+  organizationName: repoOrg,
+  projectName: repoName,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ko',
+    locales: ['ko'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl: `${repoUrl}/tree/main/doc/`,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: coverImage,
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+    navbar: {
+      title: title,
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: '시작하기',
+        },
+        {
+          href: repoUrl,
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: '시작하기',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: repoUrl,
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MJ Studio.`,
+    },
+    prism: {
+      theme: prismThemes.oceanicNext,
+      darkTheme: prismThemes.oceanicNext,
+    },
+  } satisfies Preset.ThemeConfig,
+
+  plugins: ['docusaurus-plugin-sass'],
+};
+
+export default config;
--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
-const pak = require('../packages/share/package.json');
+const fs = require('fs');
+
+const root = path.resolve(__dirname, '..');
+const packages = path.resolve(root, 'packages');
+
+// List all packages under `packages/`
+/** @type {string[]} */
+const workspaces = fs
+  .readdirSync(packages)
+  .map((p) => path.join(packages, p))
+  .filter(
+    (p) =>
+      fs.statSync(p).isDirectory() &&
+      fs.existsSync(path.join(p, 'package.json'))
+  );
+
+/** @type {Record<string, {root: string}>} */
+const dependencies = {};
+workspaces.forEach((workspace) => {
+  const pak = JSON.parse(
+    fs.readFileSync(path.join(workspace, 'package.json'), 'utf8')
+  );
+  dependencies[pak.name] = { root: workspace };
+});
 
 module.exports = {
-  dependencies: {
-    [pak.name]: {
-      root: path.join(__dirname, '../packages/share'),
-    },
-  },
+  dependencies,
 };
