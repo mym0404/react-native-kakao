cbb6f9c2b33b01bcc4dc2e741580784352c6fb95
mym0404 <mym0404@gmail.com>
2024-04-20 19:22:59 +0900

    docs: update readme

--- packages/share/src/index.tsx
+++ packages/share/src/index.tsx
@@ -1,25 +1,25 @@
-import { NativeModules, Platform } from 'react-native';
-
-const LINKING_ERROR =
-  `The package '@react-native-kakao/share' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-// @ts-expect-error
-const isTurboModuleEnabled = global.__turboModuleProxy != null;
-
-const Module = isTurboModuleEnabled
-  ? require('./spec/NativeKakaoShare').default
-  : NativeModules.KakaoShare;
-
-const Native = Module
-  ? Module
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+// import { NativeModules, Platform } from 'react-native';
+//
+// const LINKING_ERROR =
+//   `The package '@react-native-kakao/share' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo Go\n';
+//
+// // @ts-expect-error
+// const isTurboModuleEnabled = global.__turboModuleProxy != null;
+//
+// const Module = isTurboModuleEnabled
+//   ? require('./spec/NativeKakaoShare').default
+//   : NativeModules.KakaoShare;
+//
+// const Native = Module
+//   ? Module
+//   : new Proxy(
+//       {},
+//       {
+//         get() {
+//           throw new Error(LINKING_ERROR);
+//         },
+//       }
+//     );
