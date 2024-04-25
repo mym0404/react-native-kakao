df253726e28f359196e831364bf93cae251341bf
mym0404 <mym0404@gmail.com>
2024-04-25 11:13:31 +0900

    chore: update command fix #2

--- script/version.sh
+++ script/version.sh
@@ -2,10 +2,10 @@
 
 echo $1
 
-#yarn lint
-#yarn build
+yarn lint
+yarn build
 
 V=$1
 
 lerna version $V --force-publish --allow-peer-dependencies-update --changelog-include-commits-client-login " by @%l" \
-      --exact --remote-client github
+      --exact --remote-client github --create-release github
