db7ff370f74a3f9ee4cc5199d45b5d34a931a308
mym0404 <mym0404@gmail.com>
2024-04-27 01:28:28 +0900

    ci: ci

--- script/ci-android-build.sh
+++ script/ci-android-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cd example/android && ./gradlew assembleDebug --no-daemon --console=plain -PreactNativeArchitectures=arm64-v8a
+cd android && ./gradlew assembleDebug --no-daemon --console=plain -PreactNativeArchitectures=arm64-v8a
