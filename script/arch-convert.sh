7e63dd15c9d3a8c080c26fbcfb4186e937313c3a
mym0404 <mym0404@gmail.com>
2024-04-27 15:00:54 +0900

    ci: fix ci

--- script/arch-convert.sh
+++ script/arch-convert.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 # Gradle property to change
 PROPERTY="newArchEnabled"
 
@@ -11,9 +10,13 @@ CLEAN="$3"
 pwd
 # Modify app.json
 JSON="example/app.json"
+
+if [ -f $JSON ]; then
 sed -i '' -e "s/\"newArchEnabled\": true/\"newArchEnabled\": ${NEW_VALUE}/g" $JSON
 sed -i '' -e "s/\"newArchEnabled\": false/\"newArchEnabled\": ${NEW_VALUE}/g" $JSON
-
+else
+echo "${JSON} Not found"
+fi
 # Validate new value is either "true" or "false"
 if [ "${NEW_VALUE}" != "true" ] && [ "${NEW_VALUE}" != "false" ]; then
   echo "Error: The argument should be either 'true' or 'false'"
@@ -31,13 +34,16 @@ fi
 
 
 # Gradle properties file
-FILE="example/android/gradle.properties"
+GRADLE_RPOPERTIES="example/android/gradle.properties"
 
 # Create a backup of the original file
-# cp ${FILE} ${FILE}.bak
+# cp ${GRADLE_RPOPERTIES} ${GRADLE_RPOPERTIES}.bak
 
-# Use 'sed' to replace the property value
-sed -i '' -e "s/${PROPERTY}=.*/${PROPERTY}=${NEW_VALUE}/" ${FILE}
+if [ -f $GRADLE_RPOPERTIES ]; then
+sed -i '' -e "s/${PROPERTY}=.*/${PROPERTY}=${NEW_VALUE}/" ${GRADLE_RPOPERTIES}
+else
+echo "${GRADLE_RPOPERTIES} Not found"
+fi
 
 if [[ $CLEAN == 'true' ]]; then
 yarn gen:android:clean
