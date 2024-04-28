a6ceb7aa71751b61e41323b1540858b0b80f308c
mym0404 <mym0404@gmail.com>
2024-04-29 00:55:33 +0900

    build: fix codegen integer issue?

---
+++ example/src/root.js
@@ -0,0 +1,12 @@
+//index.js
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./app'); //Path with src folder
+
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
