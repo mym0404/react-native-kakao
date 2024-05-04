34e6ed3a6b62470e210d2c41e213cc0d3653e0f5
mym0404 <mym0404@gmail.com>
2024-05-04 21:52:21 +0900

    feat(share): web

--- packages/share/src/index.web.ts
+++ packages/share/src/index.web.ts
@@ -1,3 +1,4 @@
+import { filterNonNullishKeys, snakeCaseObject } from '@mj-studio/js-util';
 import { kRunWebAPI } from '@react-native-kakao/core';
 
 import type { KakaoShareAPI } from './index';
@@ -10,7 +11,63 @@ declare const Kakao: {
   API: { request: (args: any) => Promise<any> };
 };
 
-// @ts-ignore
+function createShareDefault({
+  serverCallbackArgs,
+  template,
+  objectType,
+}: {
+  objectType: string;
+  template: any;
+  serverCallbackArgs?: Record<string, string>;
+}) {
+  return kRunWebAPI(() =>
+    Kakao.Share.sendDefault({
+      objectType,
+      ...template,
+      serverCallbackArgs,
+    }),
+  );
+}
+
+function createSendDefaultMe({ template, objectType }: { objectType: string; template: any }) {
+  return kRunWebAPI(() =>
+    Kakao.API.request({
+      url: '/v2/api/talk/memo/default/send',
+      data: {
+        template_object: snakeCaseObject({
+          objectType,
+          ...template,
+        }),
+      },
+    }),
+  );
+}
+
+function createSendDefaultFriends({
+  objectType,
+  template,
+  receiverUuids,
+}: {
+  template: any;
+  objectType: any;
+  receiverUuids: string[];
+}) {
+  return kRunWebAPI(() =>
+    Kakao.API.request({
+      url: '/v1/api/talk/friends/message/default/send',
+      data: {
+        template_object: snakeCaseObject(
+          filterNonNullishKeys({
+            objectType,
+            ...template,
+          }),
+        ),
+        receiver_uuids: receiverUuids,
+      },
+    }),
+  );
+}
+
 const KakaoShare: KakaoShareAPI = {
   shareCustomTemplate: ({ templateArgs, templateId, serverCallbackArgs }) =>
     kRunWebAPI(() =>
@@ -38,6 +95,39 @@ const KakaoShare: KakaoShareAPI = {
         },
       }),
     ),
+  shareFeedTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'feed' }),
+  sendFeedTemplateToMe: ({ template }) => createSendDefaultMe({ template, objectType: 'feed' }),
+  sendFeedTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'feed' }),
+  shareListTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'list' }),
+  sendListTemplateToMe: ({ template }) => createSendDefaultMe({ template, objectType: 'list' }),
+  sendListTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'list' }),
+  shareLocationTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'location' }),
+  sendLocationTemplateToMe: ({ template }) =>
+    createSendDefaultMe({ template, objectType: 'location' }),
+  sendLocationTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'location' }),
+  shareCommerceTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'commerce' }),
+  sendCommerceTemplateToMe: ({ template }) =>
+    createSendDefaultMe({ template, objectType: 'commerce' }),
+  sendCommerceTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'commerce' }),
+  shareTextTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'text' }),
+  sendTextTemplateToMe: ({ template }) => createSendDefaultMe({ template, objectType: 'text' }),
+  sendTextTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'text' }),
+  shareCalendarTemplate: ({ template, serverCallbackArgs }) =>
+    createShareDefault({ template, serverCallbackArgs, objectType: 'calendar' }),
+  sendCalendarTemplateToMe: ({ template }) =>
+    createSendDefaultMe({ template, objectType: 'calendar' }),
+  sendCalendarTemplateToFriends: ({ template, receiverUuids }) =>
+    createSendDefaultFriends({ template, receiverUuids, objectType: 'calendar' }),
 };
 export const {
   sendCalendarTemplateToFriends,
